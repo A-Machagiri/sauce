8ab3b00ff7d56e9a440cc37f4ca94b9e13de01c3
A-Machagiri <adunurimachagiri@gmail.com>
2024-11-13 19:03:14 +0530

    Update PageObjects/Login/LoginPage_Actions.cs

--- PageObjects/Login/LoginPage_Actions.cs
+++ PageObjects/Login/LoginPage_Actions.cs
@@ -1,9 +1,35 @@
 ﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Support.UI;
+using Utilities.WebUtilities.Web_Utilities;
+
 namespace PageObjects.Login
 {
-    public partial class LoginPage
+    public partial class LoginPage_Actions
     {
+        private WebUtilities _webUtilities;
 
+        public LoginPage_Actions(WebUtilities webUtilities)
+        {
+            _webUtilities = webUtilities;
+        }
+
+        public void EnterUsername(string username)
+        {
+            _webUtilities._driver.FindElement(By.CssSelector(LoginPage_Locators.UsernameField)).SendKeys(username);
+        }
+
+        public void EnterPassword(string password)
+        {
+            _webUtilities._driver.FindElement(By.CssSelector(LoginPage_Locators.PasswordField)).SendKeys(password);
+        }
+
+        public void ClickLoginButton()
+        {
+            _webUtilities._driver.FindElement(By.CssSelector(LoginPage_Locators.LoginButton)).Click();
+        }
+
+        public string GetErrorMessage()
+        {
+            return _webUtilities._driver.FindElement(By.CssSelector(LoginPage_Locators.ErrorMessage)).Text;
+        }
     }
 }
