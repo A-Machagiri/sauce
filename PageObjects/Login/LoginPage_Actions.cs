7b59b5a6ed9f07322368ab1f8c74bb0b1b46ceb5
adunuri_machagiri <adunuri_machagiri@epam.com>
2024-11-13 18:54:58 +0530

    First-Commit

---
+++ PageObjects/Login/LoginPage_Actions.cs
@@ -0,0 +1,9 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+namespace PageObjects.Login
+{
+    public partial class LoginPage
+    {
+
+    }
+}
---
+++ PageObjects/Login/LoginPage_Locators.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageObjects.Login
+{
+    public partial class LoginPage
+    {
+        private By _username = By.Id("user-name");
+        private By _password = By.Id("password");
+        private By _loginButton = By.Id("login-button");
+    }
+}
